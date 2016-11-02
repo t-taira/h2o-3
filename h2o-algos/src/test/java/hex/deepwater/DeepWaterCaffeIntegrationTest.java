f60d15fc519c5cdc827e23635db782789b0fe7b5
Arno Candel <arno.candel@gmail.com>
2016-11-02 13:31:00 -0700

    Add Caffe integration junits.

---
+++ h2o-algos/src/test/java/hex/deepwater/DeepWaterCaffeIntegrationTest.java
@@ -0,0 +1,8 @@
+package hex.deepwater;
+
+public class DeepWaterCaffeIntegrationTest extends DeepWaterAbstractIntegrationTest {
+
+  @Override
+  DeepWaterParameters.Backend getBackend() { return DeepWaterParameters.Backend.caffe; }
+
+}
